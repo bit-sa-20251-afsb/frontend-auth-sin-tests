1d0c80093926f9d94b07d15fd0b4a8ac362d4158
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-02-20 19:44:36 -0500

    end of register component frontend tests

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RegisterComponent } from "./register.component"
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
-import { throwError } from "rxjs";
+import { of, throwError } from "rxjs";
 import Swal from "sweetalert2";
 
 describe('RegisterCompenent', ()=>{
@@ -11,6 +11,8 @@ describe('RegisterCompenent', ()=>{
     let authServiceSpy: jasmine.SpyObj<AuthService>;
     let routerSpy: jasmine.SpyObj<Router>;
 
+    const userEmail = 'email@email.com';
+    const userPassword = 'password';
     beforeEach(async ()=>{
         const authObjSpy = jasmine.createSpyObj('AuthService',['register']);
         const routerObjSpy = jasmine.createSpyObj('Router',['navigate']);
@@ -29,6 +31,33 @@ describe('RegisterCompenent', ()=>{
     it('should create', ()=>{
         expect(component).toBeTruthy();
     })
+
+    it('Deberia retornar una respuesta cuando se registra un usuario', ()=>{
+        // Arrange
+        component.email = userEmail;
+        component.password = userPassword;
+        const mockSuccessResponse = {ok: true, msg: 'Usuario registrado correctamente'};
+        authServiceSpy.register.and.returnValue(of(mockSuccessResponse));
+        // Act
+        component.onSubmit(new Event('submit'));
+        // Assert
+        expect(Swal.isVisible()).toBeTrue();
+        expect(Swal.getTitle()?.textContent).toBe('Usuer Registered!!');
+        expect(authServiceSpy.register).toHaveBeenCalledWith(userEmail,userPassword);
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['/login']);
+    })
+
+    it('Deberia manejar una respuesta response.ok en false en el componente register', ()=>{
+        // Arrange
+        const mockFailedResponse = { ok: false, error:{msg:'Hubo un error al hacer la petición de register'}};
+        authServiceSpy.register.and.returnValue(of(mockFailedResponse));
+        // Act
+        component.onSubmit(new Event('submit'));
+        // Assert
+        expect(Swal.isVisible()).toBeTrue();
+        expect(Swal.getTitle()?.textContent).toBe('Algo salió mal en el backend');
+    }
+    )
 
     it('Deberia manejar el error en el componente register', ()=>{
         // Arrange
